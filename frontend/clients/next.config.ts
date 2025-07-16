990caaa921b4368c0489484db822f9edb8b53f65
Phamthaisang <phamthaisang1710@gmail.com>
2025-07-16 17:21:51 +0700

    Initial commit

--- frontend/clients/next.config.ts
+++ frontend/clients/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["luxuryfashionbackend.onrender.com"], // 👈 Thêm dòng này
+    domains: ["localhost"], // 👈 Thêm dòng này
   },
   eslint: {
     ignoreDuringBuilds: true, // 👈 Thêm dòng này để bỏ qua lỗi ESLint khi build
